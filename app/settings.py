0c99cc7691d0dcac4e7ca93ec757a778065fcf51
Miguel Alfaro <miguel.alfaro@inventiva-solutions.com>
2023-03-14 22:25:52 -0500

    refactor: settings.py

--- app/settings.py
+++ app/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = "django-insecure-bc9)h+z&=ikl8z14hhi_ya1reb@91*a+)0r&0()!*2%y7ps1(&
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-#ALLOWED_HOSTS = ["3.85.123.242"]
+ALLOWED_HOSTS = ["3.85.123.242"]
 
 
 # Application definition
@@ -102,8 +102,15 @@ WSGI_APPLICATION = "app.wsgi.application"
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
-   
     "default": {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'westudydb',
+        'USER': 'westudyaccesspro',
+        'PASSWORD': 'O1PpjDaokF4oS9XcbjIb2fQlQ',
+        'HOST': 'westudyapp.celjmnegcr8z.us-east-1.rds.amazonaws.com',
+        'PORT': '5432',
+    },
+    "old_default": {
         "ENGINE": "django.db.backends.sqlite3",
         "NAME": BASE_DIR / "db.sqlite3",
     }
